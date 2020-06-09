2af3ed43cf48ccbf5d303e0747e3d42c93670a4c
gbdu <ogrum@live.com>
2020-06-09 13:41:00 -0600

    wetbat init

---
+++ api/src/models/contacts.model.js
@@ -0,0 +1,46 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const contacts = sequelizeClient.define('contacts', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      }
+    },
+      
+      
+      
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  contacts.associate = function (models) {
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  };
+
+  return contacts;
+};
---
+++ api/src/services/contacts/contacts.class.js
@@ -0,0 +1,5 @@
+const { Service } = require('feathers-sequelize');
+
+exports.Contacts = class Contacts extends Service {
+  
+};
