55f257eece142a92de744b1382f9db21c4d11542
gbdu <ogrum@live.com>
2020-06-12 21:13:08 -0600

    made email and phone unique for contacts

--- api/src/models/contacts.model.js
+++ api/src/models/contacts.model.js
@@ -22,11 +22,13 @@ module.exports = function (app) {
     },
     phone: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      unique: true,
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
       validate: {
         isEmail: true,
       }
